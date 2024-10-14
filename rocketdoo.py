bed06b718ef8a615f91e20362931ad7ac92d67d3
horacio1986 <horaciomontano31@gmail.com>
2024-10-14 16:47:28 -0300

    UPD: signal control

--- rocketdoo.py
+++ rocketdoo.py
@@ -8,7 +8,7 @@ VERSION = "1.0"
 
 # Maneja la interrupción con Ctrl+C
 def signal_handler(sig, frame):
-    print("\nRocketdoo ha sido cancelado.")
+    print("\nRocketdoo ha sido cancelado por el usuario.")
     sys.exit(0)
 
 # Registra el manejador de señal para SIGINT (Ctrl+C)
@@ -30,4 +30,15 @@ def main():
         return
 
     # Ejecuta Copier si no se pasa --version
-    subprocess.run(["copier", "copy", "./", "./", "--trust"], check=True)
+    try:
+        subprocess.run(["copier", "copy", "./", "./", "--trust"])
+    except KeyboardInterrupt:
+        # Este bloque captura el Ctrl+C y permite salir limpiamente
+        print("\nRocketdoo ha sido cancelado por el usuario.")
+    except Exception as e:
+        # Captura cualquier otro error, sin mostrar traceback detallado
+        print(f"\nError inesperado: {e}")
+    finally:
+        sys.exit(0)
+
+
