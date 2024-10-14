8aea8793cb6b254af9478eb49b68cb52c0ac308c
horacio1986 <horaciomontano31@gmail.com>
2024-10-14 15:14:19 -0300

    UPD: setup

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     description='This library allows you to build an automated local development environment for Odoo EE and CE.'
     long_description=open('README.md',encoding='utf-8').read(),
     author='Horacio Montaño and Elias Braceras',
-    author_email='horaciomontano@hdmsoft.com.ar'
+    author_email='horaciomontano@hdmsoft.com.ar',
+    url='https://github.com/HDM-soft/rocketdoo.git',
     packages=find_packages(),
     install_requires=[
         'copier',  # Asegúrate de incluir copier en las dependencias
