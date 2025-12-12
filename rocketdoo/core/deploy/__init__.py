67d5f0cdbbc5706b6300e35714ec4fe78cb84237
horacio1986 <horaciomontano31@gmail.com>
2025-12-12 23:05:39 +0000

    WIP: testing deploy version

--- rocketdoo/core/deploy/__init__.py
+++ rocketdoo/core/deploy/__init__.py
@@ -5,9 +5,15 @@ Deployment functionality for Odoo modules to VPS and Odoo.sh
 
 from .base import BaseDeployer, DeploymentResult
 from .config_manager import DeployConfigManager
+from .module_packager import ModulePackager
+from .vps import VPSDeployer
+from .odoo_sh import OdooSHDeployer
 
 __all__ = [
     'BaseDeployer',
     'DeploymentResult',
-    'DeployConfigManager'
+    'DeployConfigManager',
+    'ModulePackager',
+    'VPSDeployer',
+    'OdooSHDeployer'
 ]
